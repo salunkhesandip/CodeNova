1b1d17c426471ffc3001791c02e6ee52c02f9b16
Sandip Salunkhe <sandip.v.salunkhe@gmail.com>
2023-09-16 20:50:27 -0500

    Initial commit

---
+++ src/main/java/com/cleancoders/features/java/codenova/CodenovaApplication.java
@@ -0,0 +1,13 @@
+package com.cleancoders.features.java.codenova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodenovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleancoders/features/java/codenova/CodenovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleancoders.features.java.codenova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodenovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
